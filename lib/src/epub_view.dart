30443dca31da7ad0c8f6944bfbbafe7fb3ea23c9
Lednak007 <kailashkandel@gmail.com>
2021-05-13 13:38:11 +0545

    Test copy feature

--- lib/src/epub_view.dart
+++ lib/src/epub_view.dart
@@ -355,6 +355,27 @@ class _EpubViewState extends State<EpubView> {
     }
 
     final chapterIndex = _getChapterIndexBy(positionIndex: index);
+    final html = Html(
+      data: _paragraphs[index].element.outerHtml,
+      onLinkTap: (href, _, __, ___) =>
+          _onLinkPressed(href!, widget.onExternalLinkPressed),
+      style: {
+        'html': Style(
+          padding: widget.paragraphPadding as EdgeInsets?,
+        ).merge(Style.fromTextStyle(widget.textStyle)),
+      },
+      customRender: {
+        'img': (context, child, attributes, node) {
+          final url = attributes['src']!.replaceAll('../', '');
+          return Image(
+            image: MemoryImage(
+              Uint8List.fromList(
+                  widget.controller._document!.Content!.Images![url]!.Content!),
+            ),
+          );
+        }
+      },
+    );
 
     return Column(
       children: <Widget>[
@@ -362,29 +383,9 @@ class _EpubViewState extends State<EpubView> {
             _getParagraphIndexBy(positionIndex: index) == 0)
           _buildDivider(_chapters[chapterIndex]),
         SelectableText(
-          Html(
-            data: _paragraphs[index].element.outerHtml,
-            onLinkTap: (href, _, __, ___) =>
-                _onLinkPressed(href!, widget.onExternalLinkPressed),
-            style: {
-              'html': Style(
-                padding: widget.paragraphPadding as EdgeInsets?,
-              ).merge(Style.fromTextStyle(widget.textStyle)),
-            },
-            customRender: {
-              'img': (context, child, attributes, node) {
-                final url = attributes['src']!.replaceAll('../', '');
-                return Image(
-                  image: MemoryImage(
-                    Uint8List.fromList(widget
-                        .controller._document!.Content!.Images![url]!.Content!),
-                  ),
-                );
-              }
-            },
-          )..toString(),
+          html.toString(),
           toolbarOptions: ToolbarOptions(
-            copy: false,
+            copy: true,
           ),
         ),
       ],
